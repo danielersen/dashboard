9c20fd48ba8acb48b92fd09cc446d1726a79e073
Daniel Ersen <201269086+danielersen@users.noreply.github.com>
2026-05-24 23:37:43 +0200

    Update ecole_directe.js

--- backend/ecole_directe.js
+++ backend/ecole_directe.js
@@ -1,7 +1,7 @@
 export async function handleED(user, password, category) {
+  //// GERTING TOKEN
   const userAgent =
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36";
-
   // STEP 1 — GET GTK
   const gtkResponse = await fetch(
     "https://api.ecoledirecte.com/v3/login.awp?gtk=1&v=4.75.0",
@@ -15,25 +15,20 @@ export async function handleED(user, password, category) {
       }
     }
   );
-
   const setCookie =
     gtkResponse.headers.get("set-cookie");
-
   if (!setCookie) {
     throw new Error(
       "GTK cookie not found"
     );
   }
-
   const gtk =
     setCookie.match(/GTK=([^;]+)/)?.[1];
-
   if (!gtk) {
     throw new Error(
       "GTK extraction failed"
     );
   }
-
   // STEP 2 — LOGIN
   const response = await fetch(
     "https://api.ecoledirecte.com/v3/login.awp?v=4.75.0",
@@ -42,26 +37,19 @@ export async function handleED(user, password, category) {
       headers: {
         "Content-Type":
           "application/x-www-form-urlencoded",
-
         "User-Agent":
           userAgent,
-
         "X-Gtk":
           gtk,
-
         "Cookie":
           `GTK=${gtk}`,
-
         "Accept":
           "application/json, text/plain, */*",
-
         "Referer":
           "https://www.ecoledirecte.com/",
-
         "Origin":
           "https://www.ecoledirecte.com"
       },
-
       body:
         "data=" +
         encodeURIComponent(
@@ -74,20 +62,16 @@ export async function handleED(user, password, category) {
         )
     }
   );
-
   const result =
     await response.json();
-
   console.log(setCookie);
   console.log(gtk);
   console.log(result);
-
   if (result.code !== 200) {
     throw new Error(
       result.message ||
       `Login failed with code ${result.code}`
     );
   }
-
   return result.token;
 }
