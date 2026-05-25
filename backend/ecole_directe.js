5ed4810e8da94e345480b315123419fab47ee43f
Daniel Ersen <201269086+danielersen@users.noreply.github.com>
2026-05-25 09:45:32 +0200

    Return user and password in handleED function
    Add return statement to handleED function for user and password

--- backend/ecole_directe.js
+++ backend/ecole_directe.js
@@ -1,4 +1,5 @@
 export async function handleED(user, password) {
+  return `user ${user}, password ${password}`
   const userAgent =
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36";
   const gtkResponse = await fetch(
