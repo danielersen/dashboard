37839cad4af3de98b6d18d911cb80ca8db5f7fcc
Daniel Ersen <201269086+danielersen@users.noreply.github.com>
2026-06-11 07:51:03 +0200

    Update index.js

--- src/backend/ecole_directe/index.js
+++ src/backend/ecole_directe/index.js
@@ -1,4 +1,15 @@
 import { EDinformations } from "./get_informations.js";
+import { EDgrades } from "./get_grades.js"
 
-export async funtion EDfunction (env) {
+export async funtion EDfunction (env, subpath) {
+  const informations = EDinformations(env)
+  if (subpath === "info") {
+    return informations
+  } else if (subpath === "/averages") {
+    return EDgrades (env, informations)
+  } else if (subpath === "") {
+    return
+  } else {
+    return "There is no ed features which match with your request"
+  }
 }
