ec593a5277cede2933c63b4b697da9ae477d2be5
Daniel Ersen <201269086+danielersen@users.noreply.github.com>
2026-06-11 23:28:31 +0200

    Update index.js

--- src/backend/ecole_directe/index.js
+++ src/backend/ecole_directe/index.js
@@ -2,11 +2,11 @@ import { EDinformations } from "./get_informations.js";
 import { EDgrades } from "./get_grades.js"
 
 export async function EDfunction (env, subpath) {
-  const informations = EDinformations(env)
+  const informations = await EDinformations(env)
   if (subpath === "info") {
     return informations
   } else if (subpath === "averages") {
-    return EDgrades (env, informations)
+    return await EDgrades (env, informations)
   } else if (subpath === "") {
     return "None"
   } else {
