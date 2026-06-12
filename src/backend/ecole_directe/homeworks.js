6282b70d139b279dd0756296bda7d092ecf4b2c5
Daniel Ersen <201269086+danielersen@users.noreply.github.com>
2026-06-12 20:51:12 +0200

    Update homeworks.js

--- src/backend/ecole_directe/homeworks.js
+++ src/backend/ecole_directe/homeworks.js
@@ -1,4 +1,4 @@
-export async function EDhomeworks(env, informations) {
+export async function EDhomeworks(env, informations, filter) {
   const ED_USER_AGENT = env.USER_AGENT;
   const ED_VERSION = "4.75.0";
   function normalizeCookieHeader(rawCookies) {
