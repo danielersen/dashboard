f6f41215164c4f57651510520e87c2a55d10262c
Daniel Ersen <201269086+danielersen@users.noreply.github.com>
2026-06-12 19:27:55 +0200

    Add homeworks functionality to EDfunction

--- src/backend/ecole_directe/index.js
+++ src/backend/ecole_directe/index.js
@@ -1,5 +1,6 @@
 import { EDinformations } from "./get_informations.js";
 import { EDgrades } from "./get_grades.js"
+import { EDhomeworks } from "./get_homeworks.js"
 
 export async function EDfunction (env, subpath) {
   const informations = await EDinformations(env)
@@ -7,8 +8,10 @@ export async function EDfunction (env, subpath) {
     return informations
   } else if (subpath === "grades") {
     return await EDgrades (env, informations)
+  } else if (subpath === "homeworks") {
+    return await EDhomeworks (env, informations)
   } else if (subpath === "") {
-    return "None"
+    return ""
   } else {
     return "There is no ed features which match with your request"
   }
