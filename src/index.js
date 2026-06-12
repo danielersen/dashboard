65a83532e59ae75ccf06cde95784a7a4eb10244a
Daniel Ersen <201269086+danielersen@users.noreply.github.com>
2026-06-12 20:43:02 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,9 @@ import { EDfunction } from "./backend/ecole_directe/index.js";
 
 export default {
   async fetch(request, env, ctx) {
-    const url = new URL(request.url)
+    const url = new URL(request.url);
+    const body = request.text();
+    const headers = request.headers;
 
     
     // =========================
